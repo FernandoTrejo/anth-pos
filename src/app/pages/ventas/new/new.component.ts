ce982c4d349feffef75e1869264a472d98e46f31
ronaldfernandotrejo@gmail.com <ronaldfernandotrejo@gmail.com>
2023-01-14 11:19:39 -0600

    start products modal

--- src/app/pages/ventas/new/new.component.ts
+++ src/app/pages/ventas/new/new.component.ts
@@ -7,4 +7,324 @@ import { Component } from '@angular/core';
 })
 export class NewComponent {
 
+    categoriesWithProducts = this.getCategoriesWithProducts();
+    getCategoriesWithProducts(){
+      return [
+        {
+          'name': 'nombre-categoria 1',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 2',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 3',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 4',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 5',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 6',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 7',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 8',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 9',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 10',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 11',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 12',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 13',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 14',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 15',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 16',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 17',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 18',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 19',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 20',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        },
+        {
+          'name': 'nombre-categoria 21',
+          'products': [
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            },
+            {
+              'name': 'nombre-producto',
+              'existence': 15,
+              'price': '2.35'
+            }
+          ]
+        }
+      ];
+    }
 }
