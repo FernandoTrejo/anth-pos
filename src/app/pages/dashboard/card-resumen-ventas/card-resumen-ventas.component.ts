84fdd73f12777effe08758c0e8fbec5cffc801df
ronaldfernandotrejo@gmail.com <ronaldfernandotrejo@gmail.com>
2023-01-24 12:22:21 -0600

    update dashboard with real number of orders

--- src/app/pages/dashboard/card-resumen-ventas/card-resumen-ventas.component.ts
+++ src/app/pages/dashboard/card-resumen-ventas/card-resumen-ventas.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { liveQuery } from 'dexie';
+import { FindOrdersCorteActualService } from 'src/app/services/orders/find-orders-corte-actual.service';
+import { TipoTransacciones } from 'src/app/utilities/tipo_transacciones';
 
 @Component({
   selector: 'app-card-resumen-ventas',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-resumen-ventas.component.css']
 })
 export class CardResumenVentasComponent {
+  numeroVentas = liveQuery(() => this.getNumeroVentas());
+  totalVentas = liveQuery(() => this.getTotalVentas());
 
+  constructor(private findCurrentOrders : FindOrdersCorteActualService){}
+
+  async getNumeroVentas(){
+    const ventas = await this.findCurrentOrders.find(TipoTransacciones.Venta);
+    return ventas.length;
+  }
+
+  async getTotalVentas(){
+    const ventas = await this.findCurrentOrders.find(TipoTransacciones.Venta);
+    let total = 0;
+    for(let venta of ventas){
+      total += Number(venta.total);
+    }
+    return Number(total).toFixed(2);
+  }
 }
